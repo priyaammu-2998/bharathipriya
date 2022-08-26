e181592b64afc6dd335816156586e9bfc7aa4e94
bharathi <priyaammu2998@gmail.com>
2022-08-26 06:15:52 -0400

    Swapping of Two Variables

---
+++ src/bharathi/Bharathipriya.java
@@ -0,0 +1,32 @@
+package bharathi;
+
+public class Bharathipriya{
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+	    
+		
+		
+		int n1=15 , n2=30;
+		
+		int n3;
+		n3=n1;
+		n1=n2;
+		n2=n3;
+		System.out.println(n1);
+		System.out.println(n2);
+		
+				
+		
+		
+		
+
+	}
+
+}
+
