ee2bce60c9f8c064dbfb794c3d584879af92fb37
bharathi <priyaammu2998@gmail.com>
2022-08-29 04:38:44 -0400

    Split the digit

---
+++ src/bharathi/Digit.java
@@ -0,0 +1,24 @@
+package bharathi;
+
+public class Digit {
+
+	public static void main(String[] args) {
+		
+		int num=4839;
+		int a,b,c,d;
+		a=num/1000;
+		b=(num%1000)/100;
+		c=(num%100)/10;
+		d=(num%10)%10;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	
+		
+		
+		
+
+	}
+
+}
