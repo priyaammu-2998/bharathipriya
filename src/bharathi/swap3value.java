2db97d61550817239aa5a7eab08e78c5ed3d967a
bharathi <priyaammu2998@gmail.com>
2022-08-30 01:50:09 -0400

    swap 3 digit

---
+++ src/bharathi/swap3value.java
@@ -0,0 +1,27 @@
+package bharathi;
+
+public class swap3value {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=15;//x=1
+		int y=1;//y=5
+		int z=5;//z=15
+		x=x+z;//15+5=20
+		z=x-z;//20-15=5
+		x=x-z;//20-5=15
+		x=x+y;//5+1=6
+		y=x-y;//6-1=5
+		x=x-y;//6-5=1
+		System.out.println("x="+x);
+		System.out.println("y="+y);
+		System.out.println("z="+z);
+		
+		
+		
+		
+		
+	}
+
+}
