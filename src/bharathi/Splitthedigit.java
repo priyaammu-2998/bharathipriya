47a0ab1fef14e4fd5b7e99484bf3b96b53f99223
bharathi bharathi <priyaammu2998@gmail.com>
2022-08-30 00:31:56 -0400

    Split the digit

---
+++ src/bharathi/Splitthedigit.java
@@ -0,0 +1,21 @@
+package bharathi;
+
+public class Splitthedigit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num=56;
+		int a,b;
+		a=num/10;
+		b=num%10;
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+		
+		 
+
+	}
+
+}
