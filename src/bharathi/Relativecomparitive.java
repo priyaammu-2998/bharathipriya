e61d055fb5c3d7bfdc1df88d23c7422c2d86977e
bharathi <priyaammu2998@gmail.com>
2022-08-30 00:04:18 -0400

    compare program Boolean ( True, False)

---
+++ src/bharathi/Relativecomparitive.java
@@ -0,0 +1,16 @@
+package bharathi;
+
+public class Relativecomparitive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int age=18;
+		System.out.println(age>=18);
+		
+		age=18;
+				System.out.println(age>=20);
+	}
+
+}
+        
