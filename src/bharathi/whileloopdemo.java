aaff0b6bd25bcd0c3cab16d7fa5702a5f11d05fd
bharathi <priyaammu2998@gmail.com>
2022-09-06 23:46:43 -0400

    largest among three variable

---
+++ src/bharathi/whileloopdemo.java
@@ -0,0 +1,27 @@
+package bharathi;
+
+public class whileloopdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=15;
+		int b=6;
+		int c=2;
+		
+		if(a>b&&a>c)
+		System.out.println("a is a largest number");
+		else if(b>c)
+			System.out.println("b is a largest number");
+		else 
+			System.out.println("c is a largest number");
+		
+		
+		
+	   
+	   
+	    
+	}
+}
+
+
