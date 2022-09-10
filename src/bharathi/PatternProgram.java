6c5b2a04fc2a8d01635490277551d5da24bf2d90
bharathi <bharathi@Bharathi>
2022-09-10 02:02:21 -0400

    pattern pyramid

---
+++ src/bharathi/PatternProgram.java
@@ -0,0 +1,31 @@
+package bharathi;
+
+public class PatternProgram {
+
+	public static void main(String[] args) {
+		int n=3;
+		for(int i=1;i<=n;i++) {
+			for(int j=i;j<=n;j++) {
+			System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print(" *");
+			}
+			System.out.println();
+		}
+	}
+
+}		
+			
+			
+			
+			
+			
+			
+
+	
+	
+	
+	
+	
+	
