d42e14fa1b8a289b86ef54bedbef7cfa7599e92a
bharathi <priyaammu2998@gmail.com>
2022-09-13 00:16:44 -0400

    array initializtion

---
+++ src/bharathi/arrayinitialization.java
@@ -0,0 +1,23 @@
+package bharathi;
+
+public class arrayinitialization {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a[]={5,6,7,8,9,2};
+		
+		
+		for (int i=0;i<a.length;i++)
+		{
+			int c=a[i]+5;
+			System.out.println("after 5 years"+c);
+		}
+		
+		
+		
+		
+	}
+
+}
