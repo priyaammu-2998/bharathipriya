a0ccfbbb5cded12a91bca02a5a3efe5aa9efb00a
raja <raja@raja-Lenovo-E41-25>
2022-09-29 11:54:58 +0530

    pattern

---
+++ src/pattern/hollownumber.java
@@ -0,0 +1,37 @@
+package pattern;
+
+public class hollownumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n=4, p=n;
+		
+		for (int i=1;i<=n;i++) {
+		
+			for(int j=i;j<n;j++) {
+				
+				if(j==i||i==1) 
+					System.out.print(j + " " );
+					
+				
+				else {
+					System.out.print(" ");
+				}
+				
+			}                                           //incomplete
+			for(int j=p;j>n-i;j--) {
+				
+				if (i==n ||i+j==n+1)
+					System.out.print(j + " ");
+				else 
+					System.out.print(" ");
+				
+				
+		}
+			System.out.println();
+
+	}
+
+	}
+}
